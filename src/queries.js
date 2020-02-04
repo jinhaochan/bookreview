3803ae5ca6e81c2e105b391e0ca23034ed4d5f11
Chan Jin Hao <chan_jinhao@ensigninfosecurity.com>
2020-02-04 10:51:42 +0800

    trying out postgres

--- src/queries.js
+++ src/queries.js
@@ -1,11 +1,28 @@
-const Pool = require('pg').Pool
-const pool = new Pool({
-	                  user: 'adminguy',
-	                  host: 'localhost',
-	                  database: 'data',
-	                  password: 'iamtheadminnow',
-	                  port: 5432,
-})
+const { Pool } = require('pg'); 
+const secrets = require('../middleware/ENV').default;
+
+const env = process.env.NODE_ENV || 'development';
+
+let connectionString = {
+	    user: secrets.user,
+	    database: secrets.testDb,
+	    host: secrets.host
+};// checking to know the environment and suitable connection string to use
+
+if (env === 'development') {
+    connectionString.database = secrets.database;
+} else {
+    connectionString = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
+        };
+
+};
+
+const pool = new Pool(connectionString);
+
+pool.on('connect', () => console.log('connected to db'));
+
 
 const getAllData = (request, response) => {
 	          pool.query('SELECT * FROM summaries ORDER BY id ASC', (error, results) => {
